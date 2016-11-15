6188050501854c2588401355c48085d38238a7e9
jw5151 <jan.witkowski@hurra.com>
2016-11-15 15:57:43 +0100

    fix spaces for indentation

--- bin/metrics-zookeeper.rb
+++ bin/metrics-zookeeper.rb
@@ -65,77 +65,76 @@ class ZookeeperMetrics < Sensu::Plugin::Metric::CLI::Graphite
     end
 
     if response =~ /^zk_max_latency\s*(\d+)$/
-        metrics[:zk_max_latency] = Regexp.last_match(1).to_i
+      metrics[:zk_max_latency] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_min_latency\s*(\d+)$/
-        metrics[:zk_min_latency] = Regexp.last_match(1).to_i
+      metrics[:zk_min_latency] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_packets_received\s*(\d+)$/
-        metrics[:zk_packets_received] = Regexp.last_match(1).to_i
+      metrics[:zk_packets_received] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_packets_sent\s*(\d+)$/
-        metrics[:zk_packets_sent] = Regexp.last_match(1).to_i
+      metrics[:zk_packets_sent] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_num_alive_connections\s*(\d+)$/
-        metrics[:zk_num_alive_connections] = Regexp.last_match(1).to_i
+      metrics[:zk_num_alive_connections] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_outstanding_requests\s*(\d+)$/
-        metrics[:zk_outstanding_requests] = Regexp.last_match(1).to_i
+      metrics[:zk_outstanding_requests] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_server_state\s*leader$/
-        metrics[:zk_is_leader] = 1
+      metrics[:zk_is_leader] = 1
     else
-        metrics[:zk_is_leader] = 0
+      metrics[:zk_is_leader] = 0
     end
 
     if response =~ /^zk_znode_count\s*(\d+)$/
-        metrics[:zk_znode_count] = Regexp.last_match(1).to_i
+      metrics[:zk_znode_count] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_watch_count\s*(\d+)$/
-        metrics[:zk_watch_count] = Regexp.last_match(1).to_i
+      metrics[:zk_watch_count] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_ephemerals_count\s*(\d+)$/
-        metrics[:zk_ephemerals_count] = Regexp.last_match(1).to_i
+      metrics[:zk_ephemerals_count] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_approximate_data_size\s*(\d+)$/
-        metrics[:zk_approximate_data_size] = Regexp.last_match(1).to_i
+      metrics[:zk_approximate_data_size] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_open_file_descriptor_count\s*(\d+)$/
-        metrics[:zk_open_file_descriptor_count] = Regexp.last_match(1).to_i
+      metrics[:zk_open_file_descriptor_count] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_max_file_descriptor_count\s*(\d+)$/
-        metrics[:zk_max_file_descriptor_count] = Regexp.last_match(1).to_i
+      metrics[:zk_max_file_descriptor_count] = Regexp.last_match(1).to_i
     end
 
     if response =~ /^zk_followers\s*(\d+)$/
-        metrics[:zk_followers] = Regexp.last_match(1).to_i
+      metrics[:zk_followers] = Regexp.last_match(1).to_i
     else
-        metrics[:zk_followers] = 0
+      metrics[:zk_followers] = 0
     end
 
     if response =~ /^zk_synced_followers\s*(\d+)$/
-        metrics[:zk_synced_followers] = Regexp.last_match(1).to_i
+      metrics[:zk_synced_followers] = Regexp.last_match(1).to_i
     else
-        metrics[:zk_synced_followers] = 0
+      metrics[:zk_synced_followers] = 0
     end
 
     if response =~ /^zk_pending_syncs\s*(\d+)$/
-        metrics[:zk_pending_syncs] = Regexp.last_match(1).to_i
+      metrics[:zk_pending_syncs] = Regexp.last_match(1).to_i
     else
-        metrics[:zk_pending_syncs] = 0
+      metrics[:zk_pending_syncs] = 0
     end
-
 
     metrics.each do |metric, value|
       output dotted(config[:scheme], metric), value, timestamp
