38ab4ae5a57a5f5ea949913a525deaa8380052ab
jw5151 <jan.witkowski@hurra.com>
2016-11-16 09:37:58 +0100

    fix

--- bin/metrics-zookeeper.rb
+++ bin/metrics-zookeeper.rb
@@ -89,10 +89,10 @@ class ZookeeperMetrics < Sensu::Plugin::Metric::CLI::Graphite
     end
 
     metrics[:zk_is_leader] = if response =~ /^zk_server_state\s*leader$/
-      1
-    else
-      0
-    end
+                              1
+                             else
+                              0
+                             end
 
     if response =~ /^zk_znode_count\s*(\d+)$/
       metrics[:zk_znode_count] = Regexp.last_match(1).to_i
@@ -119,10 +119,10 @@ class ZookeeperMetrics < Sensu::Plugin::Metric::CLI::Graphite
     end
 
     metrics[:zk_followers] = if response =~ /^zk_followers\s*(\d+)$/
-      Regexp.last_match(1).to_i
-    else
-      0
-    end
+                               Regexp.last_match(1).to_i
+                             else
+                               0
+                             end
 
     metrics[:zk_synced_followers] = if response =~ /^zk_synced_followers\s*(\d+)$/
                                       Regexp.last_match(1).to_i
@@ -131,10 +131,10 @@ class ZookeeperMetrics < Sensu::Plugin::Metric::CLI::Graphite
                                     end
 
     metrics[:zk_pending_syncs] = if response =~ /^zk_pending_syncs\s*(\d+)$/
-      Regexp.last_match(1).to_i
-    else
-      0
-    end
+                                   Regexp.last_match(1).to_i
+                                 else
+                                   0
+                                 end
 
     metrics.each do |metric, value|
       output dotted(config[:scheme], metric), value, timestamp
