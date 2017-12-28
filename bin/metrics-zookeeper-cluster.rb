326f95f1f954fc9f70aed691f853485cf8e62054
Ben Abrams <me@benabrams.it>
2017-12-28 14:55:00 -0800

    remove extra disabled and made easier to read

--- bin/metrics-zookeeper-cluster.rb
+++ bin/metrics-zookeeper-cluster.rb
@@ -49,9 +49,10 @@ class ZookeeperMetrics < Sensu::Plugin::Metric::CLI::Graphite
          default: 2181,
          proc: proc(&:to_i)
 
-  # TODO: Remove rubocop:disable Style/CommentedKeyword on this function when fixed (https://github.com/sensu-plugins/community/issues/77#issuecomment-345813238)
-  # rubocop:disable Style/CommentedKeyword
-  def follow_url(uri_str, agent = "sensu-plugins-zookeeper/#{SensuPluginsZookeeper::Version::VER_STRING}", max_attempts = 10, timeout = 10)
+  def follow_url(uri_str,
+                 agent = "sensu-plugins-zookeeper/#{SensuPluginsZookeeper::Version::VER_STRING}",
+                 max_attempts = 10,
+                 timeout = 10)
     attempts = 0
     cookie = nil
 
