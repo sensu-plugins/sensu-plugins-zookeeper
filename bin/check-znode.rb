cc45248f6b890a2587c5933bf72dba4b3bc0101d
Matt Shields <mattboston@users.noreply.github.com>
2019-05-29 09:04:02 -0400

    Regex check for child node
    Add the ability to use regex to check for existence of a child node.

--- bin/check-znode.rb
+++ bin/check-znode.rb
@@ -18,6 +18,7 @@
 # USAGE:
 #  check if znode /test exists with value starting with 'test'
 #  ./check-znode.rb -s localhost:2181 -z /test -v '^test.*'
+#  ./check-znode.rb -s localhost:2181 -z /test -c '^test.*'
 #
 # NOTES:
 #  Multiple zk servers are accepted, e.g. zk01:2181,zk02:2181,zk03:2181
@@ -49,14 +50,26 @@ class CheckZnode < Sensu::Plugin::Check::CLI
          short: '-v',
          long: '--check_value REGEX'
 
+  option :check_child,
+         description: 'Optionally check for child node against a regex',
+         short: '-c',
+         long: '--check_child REGEX'
+  
   def znode_status
     zk = Zookeeper.new(config[:server])
     znode = zk.get(path: config[:znode])
+    children = config[:check_child] ? "#{zk.get_children(path: config[:znode])[:children]}" : null
 
     if znode[:stat].exists?
       if config[:check_value]
         if Regexp.new(config[:check_value]).match(znode[:data])
           ok "#{config[:znode]} value matched regexp '#{config[:check_value]}'"
+        elsif config[:check_child]
+          if children.include?(config[:check_child])
+            ok "#{config[:znode]} has child regexp match for '#{config[:check_child]}'"
+          else
+            critical "#{config[:znode]} doesn't have child regexp match for '#{config[:check_child]}'"
+          end
         else
           critical "#{config[:znode]} value didn't match regexp '#{config[:check_value]}'"
         end
